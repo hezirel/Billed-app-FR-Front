2e1fa2da1bc3afe6ac422f15d3fdf7fca546ffb2
hezirel <80560775+hezirel@users.noreply.github.com>
2022-03-30 16:21:16 +0200

    changing backend setup for local demo

--- src/app/Store.js
+++ src/app/Store.js
@@ -56,7 +56,7 @@ class ApiEntity {
 
 class Store {
   constructor() {
-    this.api = new Api({baseUrl: 'http://localhost:5678'})
+    this.api = new Api({baseUrl: 'http://192.168.0.52:5678'})
   }
 
   user = uid => (new ApiEntity({key: 'users', api: this.api})).select({selector: uid})
